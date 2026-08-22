e7c5b89be1a9b24a3423dbef1c7999253cde4081
TrevorWachira <greatgaming656@gmail.com>
2026-08-22 08:43:04 +0300

    Make Navbar sticky at the top of the page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 px-4 py-3">
+    <nav className="sticky top-0 z-50 bg-white dark:bg-gray-800 border-b border-gray-200 dark:border-gray-700 px-4 py-3">
       <div className="max-w-4xl mx-auto flex items-center justify-between">
         <Link to="/" className="font-bold text-lg text-indigo-600 dark:text-indigo-400">
           TBM-DeepIn
